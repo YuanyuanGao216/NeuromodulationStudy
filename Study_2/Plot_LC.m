92c31a7b6ec003cbd99c5f9482cb1087cf5f6e20
高园园 <gaoyuanyuan@gaoyuanyuans-MacBook-Pro.local>
2020-07-03 12:14:57 -0400

    update

--- Study_2/Plot_LC.m
+++ Study_2/Plot_LC.m
@@ -120,14 +120,6 @@ end
 
 end
 
-function [mean, std, mean_follow, std_follow] = cal_mean_std(Sham_data)
-mean = nanmean(Sham_data,2);
-std = 2*1.96*nanstd(Sham_data,0,2)./sqrt(sum(~isnan(Sham_data),2));
-
-follow = reshape(Sham_data(13:15,:),1,[]);
-mean_follow = nanmean(follow,2);
-std_follow = 2*1.96*nanstd(follow,0,2)./sqrt(sum(~isnan(follow),2));
-end
 
 function [y,stim,day] = y_stim_day(data,y,stim,day,stim_type)
 
