dd693eea07cc5656e8b629133ea180306f53819b
高园园 <gaoyuanyuan@gaoyuanyuans-MacBook-Pro.local>
2020-07-03 10:41:21 -0400

    clean

--- Study_2/main.m
+++ Study_2/main.m
@@ -1,60 +1,49 @@
-%
-% Copyright (c) 2015, Yarpiz (www.yarpiz.com)
-% All rights reserved. Please read the "license.txt" for license terms.
-%
-% Project Code: YPML114
-% Project Title: Implementation of Linear Discriminant Analysis in MATLAB
-% Publisher: Yarpiz (www.yarpiz.com)
-% 
-% Developer: S. Mostapha Kalami Heris (Member of Yarpiz Team)
-% 
-% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
-%
+% clear all
+clc
+close all
+close all hidden
 
-clc;
-clear;
-close all;
+%% Performance data analysis
 
-%% Load Data
+% load the performance data
+DM = ReadData;
 
-data=load('iris');
+% decide whether to delete learners
+delete_unskilled = 1;% 0 - all the learners; 1 - only skilled learners; 2 - only unskilled learners
 
-X = data.Inputs';
-T = data.Targets';
-L = vec2ind(T')';
+% Plot the learning curves
+PlotLearningCurveByDay(DM,delete_unskilled);
 
-%% Linear Discriminant Analysis (aka. Fisher Discriminant Analysis)
+% SigTest(DM,delete_unskilled);
+% PlotCUSUM(DM);
+% LearningCurveFeature(DM,delete_unskilled); %OLS
+% Paras = LearningCurve3Feature(DM,delete_unskilled); % initial level, 
+% Paras = StochasticModel(DM,delete_unskilled); 
+% Data_Analysis(Paras);
+% Data_Analysis_KFDA(Paras);
+% Data_Analysis_KFDA2(Paras); % pairwise comparision
+% Plot_2nd_day(DM,delete_unskilled,delete_high_starters)
 
-[Y, W, lambda] = LDA(X, L);
 
-%% Plot Results
-
-figure;
-
-D = size(X,2);
-for d=1:D
-    % Original Data
-    subplot(D,2,2*d-1);
-    plot(X(:,d));
-    ylabel(['x_' num2str(d)]);
-    if d==D
-        xlabel('Sample Index');
-    end
-    if d==1
-        title('Original Data');
-    end
-    grid on;
-    
-    % Transformed Data
-    subplot(D,2,2*d);
-    plot(Y(:,d));
-    ylabel(['y_' num2str(d)]);
-    if d==D
-        xlabel('Sample Index');
-    end
-    if d==1
-        title('LDA Output');
-    end
-    grid on;
-    
-end
+% % % % DM = fNIRSextract(DM);
+% % % % save('../../../Processed_Data/DataMatrix.mat','DM');
+load('../../../Processed_Data/DataMatrix.mat','DM');
+% % % % 
+% % decide_period(DM,delete_unskilled);
+period.start = 10; 
+period.end = 25; 
+MeanHbMatrixpath = MeanHb(DM,delete_unskilled,period);
+% % % % fprintf('path is %s\n',MeanHbMatrixpath)
+% % % % % % % PlotfNIRS(DM,delete_unskilled)
+% % % % % % % PlotfNIRSByDay(DM,delete_unskilled)
+% % % % % % % SigTestfNIRS(DM,delete_unskilled)
+load(MeanHbMatrixpath,'MeanHbMatrix');
+% % % 
+% Overlay(MeanHbMatrix,delete_unskilled,period)
+% Overlay_unskilled(MeanHbMatrix,delete_unskilled,period)
+Sig_Test_Hb(MeanHbMatrix,delete_unskilled,period)
+% % LOO_Test_Hb(MeanHbMatrix,delete_unskilled)
+% % load(TSHbMatrix_path,'TSHbMatrix')
+% Plot_Time_series_fNIRS
+% Demo_Safety
+% Overlay_non_responder(MeanHbMatrix,delete_unskilled,period)
