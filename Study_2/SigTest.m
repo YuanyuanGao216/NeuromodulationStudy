a17827eb8d10176ed0093cc2e13a739c402aad36
高园园 <gaoyuanyuan@gaoyuanyuans-MacBook-Pro.local>
2020-07-03 12:50:50 -0400

    update

--- Study_2/SigTest.m
+++ Study_2/SigTest.m
@@ -1,20 +1,7 @@
 function SigTest(DataMatrix,delete_unskilled)
 
-f = fieldnames(DataMatrix)';
-f{2,1} = {};
-Data_by_Sub = struct(f{:});
+Data_by_Sub = delete_sub(DataMatrix,delete_unskilled);
 
-j = 1;
-if delete_unskilled == 1
-    for i = 1:length(DataMatrix)
-        if DataMatrix(i).skilled  == 1
-            Data_by_Sub(j) = DataMatrix(i);
-            j = j + 1;
-        end
-    end
-else
-    Data_by_Sub = DataMatrix;
-end
 N = 0;
 for i = 1:length(Data_by_Sub)
     N = N + size(Data_by_Sub(i).TrainData,1);
@@ -26,8 +13,10 @@ days = zeros(N,1);
 stim = cell(N,1);
 subj = cell(N,1);
 j = 1;
-FollowUpMatrix = zeros(length(Data_by_Sub)*3,3);
-FollowUpCode = cell(length(Data_by_Sub)*3,1);
+FollowUp1 = zeros(length(Data_by_Sub),3);
+FollowUp2 = zeros(length(Data_by_Sub),3);
+FollowUp3 = zeros(length(Data_by_Sub),3);
+FollowUpCode = cell(length(Data_by_Sub),1);
 TransferMatrix = zeros(length(Data_by_Sub),2);
 TransferCode = cell(length(Data_by_Sub),1);
 for i = 1:length(Data_by_Sub)
@@ -35,7 +24,7 @@ for i = 1:length(Data_by_Sub)
     
     n = size(TrainData,1);
     time(j:j+n-1) = TrainData(:,4);
-    error(j:j+n-1) = TrainData(:,5);
+    error(j:j+n-1) = TrainData(:,5)*36;
     score(j:j+n-1) = TrainData(:,6);
     days(j:j+n-1) = TrainData(:,1);
     if string(Data_by_Sub(i).code) == 'C'
@@ -50,8 +39,10 @@ for i = 1:length(Data_by_Sub)
     j = j+n;
     
     FollowUpData = Data_by_Sub(i).FollowUpData;
-    FollowUpMatrix((i-1)*3+1:i*3,:) = FollowUpData;
-    FollowUpCode((i-1)*3+1:i*3,1) = repmat({stim_code},3,1);
+    FollowUp1(i,:) = FollowUpData(1,:);
+    FollowUp2(i,:) = FollowUpData(2,:);
+    FollowUp3(i,:) = FollowUpData(3,:);
+    FollowUpCode(i,1) = {stim_code};
     
     TransferData = Data_by_Sub(i).TransferData;
     TransferMatrix(i,:) = TransferData';
@@ -70,79 +61,139 @@ end
 exp_str = ['dep_var = ' var_type ';'];
 eval(exp_str);
 ifContinue = 1;
-while ifContinue == 1
-    prompt = 'Which day? ';
-    d = input(prompt);
-    if d<13
-        fprintf('Day #%d:\t',d)
-        index = find(days == d);
-        X = dep_var(index);
-        Group = stim(index);
-        title_string = sprintf('Day #%d %s',d,var_type);
-    elseif d == 13
-        fprintf('FollowUp:\t')
-        X = FollowUpMatrix(:,var_code);
-        Group = FollowUpCode;
-        title_string = sprintf('Follow Up %s',var_type);
-    elseif d == 14
-        fprintf('Transfer:\t')
-        X = FollowUpMatrix(:,1);
-        Group = FollowUpCode;
-        title_string = sprintf('Transfer time');
-    elseif d == 15
-        fprintf('Transfer:\t')
-        X = FollowUpMatrix(:,2);
-        Group = FollowUpCode;
-        title_string = sprintf('Transfer time');
+
+prompt = 'Which day? ';
+d = input(prompt);
+if d<13
+    fprintf('Day #%d:\t',d)
+    index = find(days == d);
+    X = dep_var(index);
+    Group = stim(index);
+    title_string = sprintf('Day #%d %s',d,var_type);
+elseif d == 131
+    fprintf('FollowUp1:\t')
+    X = FollowUp1(:,var_code);
+    Group = FollowUpCode;
+    title_string = sprintf('Follow Up 1 %s',var_type);
+elseif d == 132
+    fprintf('FollowUp2:\t')
+    X = FollowUp2(:,var_code);
+    Group = FollowUpCode;
+    title_string = sprintf('Follow Up 2 %s',var_type);
+elseif d == 133
+    fprintf('FollowUp3:\t')
+    X = FollowUp3(:,var_code);
+    Group = FollowUpCode;
+    title_string = sprintf('Follow Up 3 %s',var_type);
+elseif d == 13
+    fprintf('FollowUp:\t')
+    X = [FollowUp1(:,var_code);FollowUp2(:,var_code);FollowUp3(:,var_code)];
+    if var_code == 2
+        X = X*36;
     end
-    [p,~,stats] = anova1(X,Group,'on');
-    fprintf('Anova p = %.3f:\n',p)
-    [c,m,h,nms] = multcompare(stats);
-    display([nms(c(:,1)), nms(c(:,2)), num2cell(c(:,3:6))])
+    Group = repmat(FollowUpCode,3,1);
+    title_string = sprintf('Follow Up %s',var_type);
     figure
     hold on
-    boxplot(X,Group,'GroupOrder',{'tDCS','tRNS','Sham'})
-    hold on
-    yt = get(gca, 'YTick');
-    y_limits = ylim;
-    xt = get(gca, 'XTick');
-    
-    
-    alpha = 0.05;
-    post_hoc_p = c(:,6);
-    for i = 1:length(post_hoc_p)
-        p_value = c(i,6);
-        stim1 = string(nms(c(i,1)));
-        stim2 = string(nms(c(i,2)));
-        if p_value < alpha
-            if (strcmp(stim1,'tDCS')&& strcmp(stim2,'Sham'))||(strcmp(stim2,'tDCS')&& strcmp(stim1,'Sham'))
-                axis([xlim    y_limits(1)  ceil(max(yt)*1.8)])
-            else
-                axis([xlim    y_limits(1)  ceil(max(yt)*1.5)])
-            end
-            if (strcmp(stim1,'tDCS')&& strcmp(stim2,'Sham'))||(strcmp(stim2,'tDCS')&& strcmp(stim1,'Sham'))
-                axis([xlim    y_limits(1)  ceil(max(yt)*1.8)])
-                plot(xt([1 3]), [1 1]*max(yt)*1.5, '-k')
-                text(xt(2)-0.3,max(yt)*1.6,sprintf('p=%.3f%s',p_value,'*'),'FontSize',10)
-            elseif (strcmp(stim1,'tRNS')&& strcmp(stim2,'Sham'))||(strcmp(stim2,'tRNS')&& strcmp(stim1,'Sham'))
-                axis([xlim    y_limits(1)  ceil(max(yt)*1.5)])
-                plot(xt([2 3]), [1 1]*max(yt)*1.3, '-k')
-                text((xt(2)+xt(3))/2-0.3,max(yt)*1.4,sprintf('p=%.3f%s',p_value,'*'),'FontSize',10)
-            elseif (strcmp(stim1,'tDCS')&& strcmp(stim2,'tRNS'))||(strcmp(stim2,'tDCS')&& strcmp(stim1,'tRNS'))
-                axis([xlim    y_limits(1)  ceil(max(yt)*1.5)])
-                plot(xt([1 2]), [1 1]*max(yt)*1.1, '-k')
-                text((xt(1)+xt(2))/2-0.3,max(yt)*1.2,sprintf('p=%.3f%s',p_value,'*'),'FontSize',10)
-            end
-        end
-    end
-
-    
-    hold off
-    title(title_string)
+    plot(1*ones(length(X(strcmp(Group,'tDCS'))),1),X(strcmp(Group,'tDCS')),'b*')
+    plot(2*ones(length(X(strcmp(Group,'tRNS'))),1),X(strcmp(Group,'tRNS')),'r*')
+    plot(3*ones(length(X(strcmp(Group,'Sham'))),1),X(strcmp(Group,'Sham')),'k*')
     set(gcf, 'Position',  [100, 100, 250, 200])
+    xticks(1:3)
+    xticklabels({'tDCS','tRNS','Sham'})
+    xtickangle(90)
+    xlim([0.5 3.5])
     set(gca,'FontSize',12)
-    
-    ifContinue = input('Continue? ');
-    close all
-    close all hidden
+    title(title_string)
+elseif d == 14
+    fprintf('Transfer1:\t')
+    X = TransferMatrix(:,1);
+    Group = TransferCode;
+    title_string = sprintf('Transfer#1 time');
+    figure
+    hold on
+    plot(1*ones(length(X(strcmp(TransferCode,'tDCS'))),1),X(strcmp(TransferCode,'tDCS')),'b*')
+    plot(2*ones(length(X(strcmp(TransferCode,'tRNS'))),1),X(strcmp(TransferCode,'tRNS')),'r*')
+    plot(3*ones(length(X(strcmp(TransferCode,'Sham'))),1),X(strcmp(TransferCode,'Sham')),'k*')
+    set(gcf, 'Position',  [100, 100, 250, 200])
+    xticks(1:3)
+    xticklabels({'tDCS','tRNS','Sham'})
+    xlim([0.5 3.5])
+    set(gca,'FontSize',12)
+    title(title_string)
+elseif d == 15
+    fprintf('Transfer2:\t')
+    X = TransferMatrix(:,2);
+    Group = TransferCode;
+    title_string = sprintf('Transfer#2 time');
+    figure
+    hold on
+    plot(1*ones(length(X(strcmp(TransferCode,'tDCS'))),1),X(strcmp(TransferCode,'tDCS')),'b*')
+    plot(2*ones(length(X(strcmp(TransferCode,'tRNS'))),1),X(strcmp(TransferCode,'tRNS')),'r*')
+    plot(3*ones(length(X(strcmp(TransferCode,'Sham'))),1),X(strcmp(TransferCode,'Sham')),'k*')
+    set(gcf, 'Position',  [100, 100, 250, 200])
+    xticks(1:3)
+    xticklabels({'tDCS','tRNS','Sham'})
+    xlim([0.5 3.5])
+    set(gca,'FontSize',12)
+    title(title_string)
 end
+[p,~,stats] = anova1(X,Group,'on');
+fprintf('Anova p = %.3f:\n',p)
+[c,m,h,nms] = multcompare(stats);
+display([nms(c(:,1)), nms(c(:,2)), num2cell(c(:,3:6))])
+figure
+hold on
+boxplot(X,Group,'GroupOrder',{'tDCS','tRNS','Sham'},'colors','rbk','symbol','k.','outliersize',3)
+xtickangle(45)
+hold on
+% yt = get(gca, 'YTick');
+% y_limits = ylim;
+% xt = get(gca, 'XTick');
+% 
+% 
+% alpha = 0.05;
+% post_hoc_p = c(:,6);
+%     for i = 1:length(post_hoc_p)
+%         p_value = c(i,6);
+%         stim1 = string(nms(c(i,1)));
+%         stim2 = string(nms(c(i,2)));
+%         if p_value < alpha
+%             if (strcmp(stim1,'tDCS')&& strcmp(stim2,'Sham'))||(strcmp(stim2,'tDCS')&& strcmp(stim1,'Sham'))
+%                 axis([xlim    y_limits(1)  ceil(max(yt)*1.8)])
+%             else
+%                 axis([xlim    y_limits(1)  ceil(max(yt)*1.5)])
+%             end
+%             if (strcmp(stim1,'tDCS')&& strcmp(stim2,'Sham'))||(strcmp(stim2,'tDCS')&& strcmp(stim1,'Sham'))
+%                 axis([xlim    y_limits(1)  ceil(max(yt)*1.8)])
+%                 plot(xt([1 3]), [1 1]*max(yt)*1.5, '-k')
+%                 text(xt(2)-0.3,max(yt)*1.6,sprintf('p=%.3f%s',p_value,'*'),'FontSize',10)
+%             elseif (strcmp(stim1,'tRNS')&& strcmp(stim2,'Sham'))||(strcmp(stim2,'tRNS')&& strcmp(stim1,'Sham'))
+%                 axis([xlim    y_limits(1)  ceil(max(yt)*1.5)])
+%                 plot(xt([2 3]), [1 1]*max(yt)*1.3, '-k')
+%                 text((xt(2)+xt(3))/2-0.3,max(yt)*1.4,sprintf('p=%.3f%s',p_value,'*'),'FontSize',10)
+%             elseif (strcmp(stim1,'tDCS')&& strcmp(stim2,'tRNS'))||(strcmp(stim2,'tDCS')&& strcmp(stim1,'tRNS'))
+%                 axis([xlim    y_limits(1)  ceil(max(yt)*1.5)])
+%                 plot(xt([1 2]), [1 1]*max(yt)*1.1, '-k')
+%                 text((xt(1)+xt(2))/2-0.3,max(yt)*1.2,sprintf('p=%.3f%s',p_value,'*'),'FontSize',10)
+%             end
+%         end
+%     end
+
+
+hold off
+%     title(title_string)
+ax1 = gca;
+% ax1_old_pos=ax1.Position;
+% Yscale = 0.8;
+% ax1.Position=[ax1_old_pos(1)*Yscale ax1_old_pos(2) ax1_old_pos(3) ax1_old_pos(4)];
+ax1.Position = [0.2,0.3,0.72,0.67];
+set(gcf, 'Position',  [100   234    115    96])
+set(gca,'FontSize',8)
+set(gca, 'FontName', 'helvetica')
+
+box off
+
+%     ifContinue = input('Continue? ');
+%     close all
+%     close all hidden
